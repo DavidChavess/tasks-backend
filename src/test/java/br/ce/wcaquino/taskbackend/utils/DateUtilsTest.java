7f98a0cfa8646571d67220b23dcbe1b6beb0c138
David Chaves Ferreira <davi.ch.fe@gmail.com>
2021-02-11 20:26:51 -0300

    Testando a classe date utils

---
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -0,0 +1,26 @@
+package br.ce.wcaquino.taskbackend.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+public class DateUtilsTest {
+    @Test
+    public void deveRetornarTrueParaDatasFuturas(){
+        LocalDate dataFutura = LocalDate.of(2030,01,01);
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(dataFutura));
+    }
+
+    @Test
+    public void deveRetornarFalseParaDatasPassadas(){
+        LocalDate dataPassada = LocalDate.of(2015,01,01);
+        Assert.assertFalse(DateUtils.isEqualOrFutureDate(dataPassada));
+    }
+
+    @Test
+    public void deveRetornarTrueParaDatasAtuais(){
+        LocalDate dataAtual = LocalDate.now();
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(dataAtual));
+    }
+}
